cb42bf5824f7952ec2438819011dff070d7eb43b
emmanuelist <emmanuel.paul75@yahoo.com>
2022-10-28 19:08:07 +0100

    updated

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - main function
- * Return: Always 0
- */
 int main(void)
 {
 	int n;
@@ -12,7 +8,6 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-
 	/*
 	 * write your line of code here...
 	 * Remember:
