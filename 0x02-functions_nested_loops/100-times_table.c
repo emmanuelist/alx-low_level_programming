602303a7c6b0ac4dd10e77175b041107883a7f42
emmanuelist <emmanuel.paul75@yahoo.com>
2023-05-26 01:22:38 +0100

    Multiple mains

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,18 +9,25 @@
 
 void print_times_table(int n)
 {
-	int i, j;
+	int i, j, result;
 
 	if (n < 0 || n > 15)
+	{
+
 		return; /* Exit the function if n is out of range */
+	}
 
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
 		{
-			int result = i * j;
+			result = i * j;
+			if (j != 0)
+			{
+				printf(", "); /* Separate numbers with  comm nd three spaces */
+			}
 
-			printf("%-4d", result); /* Adjust the width foor neat aligment */
+			printf("%-3d", result); /*Print the number left-aligned with a width of 3 chracters*/
 		}
 		printf("\n");
 	}
