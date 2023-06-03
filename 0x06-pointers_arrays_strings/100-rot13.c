e997efbcbb2e33d4d837945890f4058ec755858c
emmanuelist <emmanuel.paul75@yahoo.com>
2023-06-03 02:33:24 +0100

    Function that encodes a string using rot13.

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <string.h>
-#include <ctype.h>
 /**
  * rot13 - Encodes a string using ROT13.
  * @str: The string to encode.
@@ -9,23 +7,16 @@
  */
 char *rot13(char *str)
 {
-	int i, j;
-	char letters[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	char rot13[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	/* Iterate over each character in the string */
-	for (i = 0; str[i] != '\0'; i++)
+	char *ptr = str;
+
+	while (*ptr)
 	{
-		/* Check if the current character */
-		if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
-		{
-			/* Find the corresponding letter in thne ROT13 mapping */
-			for (j = 0; letters[j] != '\0'; j++)
-			{
-				if (str[i] == letters[j])
-					str[i] = rot13[j];
-				break; /* Found a match, no need to continue searching */
-			}
-		}
+		if ((*ptr >= 'a' && *ptr <= 'm') || (*ptr >= 'A' && *ptr <= 'M'))
+			*ptr += 13;
+		else if ((*ptr >= 'm' && *ptr <=  'z') || (*ptr >= 'N' && *ptr <= 'Z'))
+			*ptr -= 13;
+
+		ptr++;
 	}
 
 	return (str);
