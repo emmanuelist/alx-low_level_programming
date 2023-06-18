eb204cac603f6ca6743d35d03636a58be2a09a35
emmanuelist <emmanuel.paul75@yahoo.com>
2023-06-19 00:30:21 +0100

    Function that returns 1 if a string is a palindrome and 0 if not.

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,43 +1,48 @@
 #include "main.h"
 #include <string.h>
+#include <stdbool.h>
 
 /**
- * s_len - calculates the length of  string.
- * @s: original string
+ * is_palindrome_helper - Recursive helper function
+ * to check if a string is a palindrome
+ * @s: The string to check
+ * @start: The starting index of the current comparison
+ * @end: The ending index of the current comparison
  *
- * Return: length of string
+ * Return: true if the string is a palindrome, false otherwise
  */
+bool is_palindrome_helper(char *s, int start, int end)
+{
+	/* Base case: when start and end pointers meet or cross each other */
+	if (start >= end)
+		return (true);
 
+	/**
+	* Recursive case: check if the characters
+	* at start and end positions are equal
+	*/
+	if (s[start] != s[end])
+		return (false);
+
+	/* Recursively call the function with updated pointers */
+	return (is_palindrome_helper(s, start + 1, end - 1));
+}
+
+/**
+ * is_palindrome - Check if a string is a palindrome
+ * @s: The string to check
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise
+ */
 int is_palindrome(char *s)
 {
-	/* calculate the length of the string */
+	/* Calculate the length of the string */
 	int len = strlen(s);
 
-	/* Initialize two pointers, one at the beginning and and one at the end */
-	int start = 0;
-	int end = len - 1;
-
-
-	/* Check for empty string (considered a palindrome */
+	/* Check for empty string (considered a palindrome) */
 	if (len == 0)
-	{
-		return (1);
-	}
+		(return) 1;
 
-	
-	/* Iterate until the two pointers meet or cross each other */
-	while (start < end)
-	{
-		/* Compare the characters at the current positions */
-		if (s[start] != s[end])
-		{
-			return (0); /* Not a Palindrome */
-		}
-
-		/* Move the pointers closer to each other */
-		start++;
-		end--;
-	}
-
-		return (1); /* Palindrome */
+	/* Start the recursive helper function */
+	return (is_palindrome_helper(s, 0, len - 1) ? 1 : 0);
 }
