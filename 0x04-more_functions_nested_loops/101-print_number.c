f85b54e5b6fdc0ee46eb62e365b9c40a879c1811
emmanuelist <emmanuel.paul75@yahoo.com>
2023-06-22 23:42:26 +0100

    Modify code to give expected output

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,10 +9,19 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		/* Handle the case for y=the largest negative integer */
+		if (n == -2147483648)
+		{
+			_putchar('2');
+			n = 147483648;
+		}
+		else
+		{
+			n = -n;
+		}
 	}
 
-	if (n / 10)
+	if (n / 10 != 0)
 		print_number(n / 10);
 
 	_putchar(n % 10 + '0');
