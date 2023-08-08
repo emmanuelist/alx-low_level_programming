d80c5dc90f58fce94b4d4e326b552b77e5b24477
emmanuelist <emmanuel.paul75@yahoo.com>
2023-08-08 13:31:59 +0100

    Modify code with helper function

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,31 +1,47 @@
 /**
- * is_palindrome - Checks if a string is a palindrome.
+ * palindrome_recursive - Recursive helper function to check if a string is a palindrome.
  * @s: The string to be checked.
- *
- * Return: 1 if the string is palindrome, 0 otherwise.
+ * @start: The starting index of the current comparison.
+ * @end: The ending index of the cureent comparison.
+ * Return: 1 if the string is a palindrome, 0 otherwiw.
  */
 
-int is_palindrome(char *s)
+int palindrome_recursive(char *s, int start, int end)
 {
+	if (start >= end)
+	{
+		return (1); /* Base case: if indices meet or cross, string is palindrome */
+	}
+
+	if (s[start] != s[end])
+	{
+		return (0); /* Chracters don't match, nt a palindrome */
+	}
+
+	/* Recurse with incremented start and decremented end */
+	return (palindrome_recursive(s, start + 1, end - 1));
+}
+
+/**
+ * is_palindrome -Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ */
+
+int is_palindrome(char *s) {
 	int len = 0;
-	int i;
 
 	/* Calculate the length of the string */
 	while (s[len] != '\0')
+	{
 		len++;
+	}
 
 	/* Check if the string is empty */
 	if (len == 0)
-		return (1);
-
-	/* Iterate through the first half of the string */
-	for (i = 0;  i < len / 2; i++)
 	{
-		/* Compare charcters at corresponding positions from both ends */
-		if (s[i] != s[len - i - 1])
-			return (0);
+		return (1);
 	}
 
-	return (1);
+	/* Call the recursive helper function with start and end indices */
+	return (palindrome_recursive(s, 0, len -1));
 }
-
