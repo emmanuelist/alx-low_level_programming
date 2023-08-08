3abe87639989bfd4d0a972d2e9acb5fea8005de1
emmanuelist <emmanuel.paul75@yahoo.com>
2023-08-08 11:24:20 +0100

    Function that returns 1 if the input integer is a prime number, otherwise return 0.

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,24 @@
+/**
+ * is_prime_number - Checks if a number is a prime number.
+ * @n: The number to be checked.
+ * Return: 1 if the number is prime, 0 if it's not.
+ */
+
+int is_prime_number(int n)
+{
+	int i;
+
+	/* 1 and negative numbers are not prime */
+	if (n <= 1)
+		return (0);
+
+	/* Check if n is divisible by any number less thn itself */
+	for ( i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+			return (0);
+	}
+
+	/* n is prime if it is not divisible by any number less than itself */
+	return (1);
+}
