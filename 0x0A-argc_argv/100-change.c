e5b1278dde5b1c3a98ec90dc7f1bc7563898ba5b
emmanuelist <emmanuel.paul75@yahoo.com>
2023-08-10 14:02:35 +0100

    Make changes to the code

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,49 +1,54 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - Entry point of the program
- * @argc: The number of command line arguments
- * @argv: An array of strings containing the arguments
- * Return: 0 on success, 1 on error
+ * main - Prints the minimum number of coins to
+ *        make change for an amount of money.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: If the number of arguments is not exactly one - 1.
+ *         Otherwise - 0.
  */
 int main(int argc, char *argv[])
 {
-	/* Check if we have exactly 1 argument */
+	int cents, coins = 0;
+
 	if (argc != 2)
 	{
-		printf("Oops! You need to provide an amount in cents.\n");
+		printf("Error\n");
 		return (1);
 	}
 
-	/* Convert the user's input into an integer */
-	int cents = atoi(argv[1]);
+	cents = atoi(argv[1]);
 
-	/* Check if the user tried to be tricky with a negative amount */
-	if (cents < 0)
+	while (cents > 0)
 	{
-		printf("Hmm, can't make change for a negative amount. So, it's 0 cents.\n");
-		return (0);
+		coins++;
+		if ((cents - 25) >= 0)
+		{
+			cents -= 25;
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
+			cents -= 10;
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
+			cents -= 5;
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
+			cents -= 2;
+			continue;
+		}
+		cents--;
 	}
 
-	/* These are the coins we have */
-	int coins[] = {25, 10, 5, 2, 1};
-	int numCoins = sizeof(coins) / sizeof(coins[0]);
-
-	int count = 0; /* Initialize the counter for coins used */
-
-	/* Let's figure out the coins needed */
-	int i;
-
-	for (i = 0; i < numCoins; i++)
-	{
-		count += cents / coins[i]; /* Count how many of this coin we can use */
-		cents %= coins[i]; /* Get the remaining amount after using this coin */
-	}
-
-	/* Print the result */
-	printf("You need the minimum of %d coins.\n", count);
+	printf("%d\n", coins);
 
 	return (0);
 }
