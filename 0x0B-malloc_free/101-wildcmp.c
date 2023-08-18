b5f70278c6e52f35a04dda8b96934f1c91b88094
emmanuelist <emmanuel.paul75@yahoo.com>
2023-08-18 12:20:15 +0100

    Add untrack files

---
+++ 0x0B-malloc_free/101-wildcmp.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - Creates an array of characters and fills it with a specific character.
+ * @size: The size of the array to create.
+ * @c: The character to fill the array with.
+ *
+ * Return: A pointer to the created array (Success), NULL (Error).
+ */
+char *create_array(unsigned int size, char c)
+{
+    /**
+     * Check if the requested size is 0, which is not allowed.
+    */
+    if (size == 0)
+        return (NULL);
+
+    /**
+     * Allocate memory for the array.
+    */
+    char *array = (char *)malloc(sizeof(char) * size);
+
+    /**
+     * Check if memory allocation was successful.
+    */
+    if (array == NULL)
+        return (NULL);
+
+    /**
+     * Fill the array with the specified character.
+    */
+    for (unsigned int i = 0; i < size; i++)
+    {
+        array[i] = c;
+    }
+
+    /**
+     * Add a null-terminating character to make it a valid string.
+    */
+    array[size] = '\0';
+
+    return (array);
+}
