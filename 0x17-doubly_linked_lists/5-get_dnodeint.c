fa2436a3eb5e40a6380b77a9555294ff0e147e77
emmanuelist <emmanuel.paul75@yahoo.com>
2023-10-14 02:55:40 +0100

    Fix indentation issue

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -8,13 +8,14 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-              dlistint_t *current = head;
-              dlistint_t *next;
+	dlistint_t *current = head;
+	dlistint_t *next;
 
-              while (current != NULL)
-              {
-                             next = current->next;
-                             free(current);
-                             current = next;
-              }
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+
+		current = next;
+	}
 }
